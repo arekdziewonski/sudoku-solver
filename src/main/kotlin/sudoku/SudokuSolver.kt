c4f0263bc4cabe3dad1774df8582fc0f31ec0469
arek dziewonski <7753142+arekdziewonski@users.noreply.github.com>
2021-01-23 16:03:16 +0000

    Display usage and exit on a missing parameter

--- src/main/kotlin/sudoku/SudokuSolver.kt
+++ src/main/kotlin/sudoku/SudokuSolver.kt
@@ -2,8 +2,13 @@ package sudoku
 
 import java.io.FileInputStream
 import java.nio.file.Paths
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
+    if (args.size != 1) {
+        println("usage: SudokuSolver <file-with-initial-grid>")
+        exitProcess(1)
+    }
     val currentDir = Paths.get("").toAbsolutePath().toFile()
 
     val content = FileReader(FileInputStream(args[0])).contentAsSingleLine()
